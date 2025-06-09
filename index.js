55cf0fb730b7ea99af58c5201dbb29fe77852df8
Yamanayya79 <108881704+Yamanayya79@users.noreply.github.com>
2025-06-09 14:15:05 +0530

    Update index.js
    url changed in cors

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const PORT =process.env.PORT || 5000
 
 // app.use(cors());
 app.use(cors({
-  origin: 'http://localhost:3000', // frontend URL
+  origin: 'https://nandiagri2025-git-main-yamanayyas-projects.vercel.app/', // frontend URL
   credentials: true
 }));
 // app.use(express.json());
